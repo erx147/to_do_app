0036b17c1914dfec0e5c4e7c57839ceb11b77f79
Jorden Cook <jcookfims@gmail.com>
2023-10-15 21:00:52 -0500

    Updated Dependencies for File and REST support

--- lib/providers/task.dart
+++ lib/providers/task.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-
-//Everything the user adds to the list is a task.
-//Task provider is self explanatory and its job is being the provider for the project.
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 class Task {
   final String id;
@@ -17,56 +18,82 @@ class Task {
     required this.dueTime,
     this.isDone = false,
   });
+
+  // Convert a Task object into a Map object
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'dueDate': dueDate.toIso8601String(),
+      'dueTime': dueTime.format(TimeOfDay.now()
+          .toString() as BuildContext),
+      'isDone': isDone,
+    };
+  }
+
+  // Convert a Map object into a Task object
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      description: map['description'],
+      dueDate: DateTime.parse(map['dueDate']),
+      dueTime: TimeOfDay.fromDateTime(DateTime.parse(map['dueTime'])),
+      isDone: map['isDone'],
+    );
+  }
 }
 
 class TaskProvider with ChangeNotifier {
+  List<Task> _toDoList = [];
+
   List<Task> get itemsList {
     return _toDoList;
   }
 
-  final List<Task> _toDoList = [
-    Task(
-      id: 'task#1',
-      description: 'Create my models',
-      dueDate: DateTime.now(),
-      dueTime: TimeOfDay.now(),
-    ),
-    Task(
-      id: 'task#2',
-      description: 'Add provider',
-      dueDate: DateTime.now(),
-      dueTime: TimeOfDay.now(),
-    ),
-  ];
+  final _baseUrl =
+      'https://your-api-endpoint.com/tasks'; // Replace with API endpoint
 
-  Task getById(String id) {
-    return _toDoList.firstWhere((task) => task.id == id);
+  Future<File> get _localFile async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/tasks.json');
   }
 
-  void createNewTask(Task task) {
-    final newTask = Task(
-      id: task.id,
-      description: task.description,
-      dueDate: task.dueDate,
-      dueTime: task.dueTime,
+  Future<void> saveTasks() async {
+    final file = await _localFile;
+    final jsonString =
+        json.encode(_toDoList.map((task) => task.toMap()).toList());
+    await file.writeAsString(jsonString);
+  }
+
+  Future<void> loadTasks() async {
+    try {
+      final file = await _localFile;
+      final jsonString = await file.readAsString();
+      final jsonData = json.decode(jsonString) as List;
+      _toDoList = jsonData.map((taskData) => Task.fromMap(taskData)).toList();
+      notifyListeners();
+    } catch (error) {
+      print('Error loading tasks: $error');
+    }
+  }
+
+  Future<void> fetchTasksFromAPI() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+    final jsonData = json.decode(response.body) as List;
+    _toDoList = jsonData.map((taskData) => Task.fromMap(taskData)).toList();
+    notifyListeners();
+  }
+
+  Future<void> addTaskToAPI(Task task) async {
+    final response = await http.post(
+      Uri.parse(_baseUrl),
+      body: json.encode(task.toMap()),
     );
-    _toDoList.add(newTask);
-    notifyListeners();
+    if (response.statusCode == 201) {
+      _toDoList.add(task);
+      notifyListeners();
+    }
   }
 
-  void editTask(Task task) {
-    removeTask(task.id);
-    createNewTask(task);
-  }
-
-  void removeTask(String id) {
-    _toDoList.removeWhere((task) => task.id == id);
-    notifyListeners();
-  }
-
-  void changeStatus(String id) {
-    int index = _toDoList.indexWhere((task) => task.id == id);
-    _toDoList[index].isDone = !_toDoList[index].isDone;
-    //print('PROVIDER ${_toDoList[index].isDone.toString()}');
-  }
+  // Similarly, you can add methods to update and delete tasks using PUT and DELETE HTTP methods.
 }
